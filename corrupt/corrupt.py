0b1aa714139f72c57494251ca9dd412aec532684
astar <jaroslav.vazny@gmail.com>
2012-03-03 18:44:57 +0100

    glob for filter *.fits

--- corrupt/corrupt.py
+++ corrupt/corrupt.py
@@ -3,18 +3,25 @@
 This should corespond to the corrupted spectrum """
 
 
-import sys, pyfits, numpy as np, os
+import sys, pyfits, numpy as np, os, glob
 
 def main():
 
     Halpha = 6564.614
+    range = 5
 
     if len(sys.argv) > 1:
-        file = sys.argv[1] 
+        dir = sys.argv[1] 
         logFile = sys.argv[2]
     else:
         sys.exit(1)
+    
+    files = listdir(dir)
+    for file in files:
+        print file
 
+def check(file):
+    """Check fits file for zeros in inverse varience """
     hdu = pyfits.open(file)
     data = hdu[1].data
     header = hdu[1].header
@@ -22,7 +29,7 @@ def main():
     sigma = data.field('inverse_variance')
     x = data.field('wavelength')
     
-    range = 5
+    
     sigma2 = sigma[(x < Halpha + range) & (x > Halpha - range)]
 
     if not sigma2.any():
@@ -30,13 +37,14 @@ def main():
             z = len(sigma)
         else:
             z = zeros(sigma,x,Halpha)
-            
+
+def log(logFile,row):            
         f = open(logFile + '.log','a')
         f.write(row([file, str(z), str(header['MJD']),str(header['PLATEID']),str(header['FIBERID'])]))
         f.close()
 
 def listdir(d):
-    return [os.path.join(d, f) for f in os.listdir(d)]
+    return [os.path.join(d, f) for f in glob.glob(d + '/*.fits')]
 
 def zeros(sigma,x, Halpha):
     """ Test how many zeros are to the left """
