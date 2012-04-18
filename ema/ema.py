2e172bf2d63d96ea1af35718a7c7e87af90b00c9
astar <jaroslav.vazny@gmail.com>
2012-04-18 19:53:22 +0200

    ema working, one plot per star only

--- ema/ema.py
+++ ema/ema.py
@@ -19,65 +19,117 @@ __version__ = '1.2.3' #Versioning: http://www.python.org/dev/peps/pep-0386/
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
+import csv
 import re
 
-class Spectra():
-    """ manipulate spectra """
+class Stars():
+    """ """
 
-    def __init__(self):
-        self.stars = ['a', 'b', 'c', 'd', 'e']
-
-
+    def __init__(self, file_name):
+        self.file_name = file_name
+        stars_file = File(self.file_name)
+        self.csv_data = stars_file.open_csv()
+        self.get_stars()
         self.position = 0
-        self.get_spectra()
-        self.current = self.spectrum[self.position]
+        self.current = self.star[self.position]
 
     def next(self):
-        if self.position < len(self.stars) - 1:
+        if self.position < len(self.csv_data) - 1:
             self.position += 1
-            self.current = self.spectrum[self.position]
+            self.current = self.star[self.position]
 
     def previous(self):
         if self.position > 0:
             self.position -= 1
-            self.current = self.spectrum[self.position]
+            self.current = self.star[self.position]
 
-    def get_spectra(self):
-        self.spectrum = []
-        for i, star in enumerate(self.stars):
-            # test data
-            x = np.arange(10)
-            y = x**i
-            self.spectrum.append(Spectrum(star, i, i+1, x, y))
 
-    def open_file(self, name):
+
+    def get_stars(self):
+        self.star = []
+        for d in self.csv_data:
+            name = d[0]
+            ra   = d[1]
+            dec  = d[2]
+            file_name = self.get_file_name()
+            self.star.append(Star(file_name, name, ra, dec))
+
+    def get_file_name(self):
+        return 'votable.xml'
+
+
+
+
+class File():
+    def __init__(self, name):
+        self.name = name
+
+    def open_file(self):
         try:
-            f = open(name, "r+")
+            f = open(self.name, "r+")
             text = f.read()
             f.close()
             return text
         except IOError:
             print "Could not open file!"
 
-    def parse_fits(self, text):
+    def open_csv(self):
+        try:
+            f = csv.reader(open(self.name, "rU"))
+            data = []
+            data.extend(f)
+            print data
+            return data
+
+        except IOError:
+            print "Could not open file!"
+
+
+
+class Star():
+    """  """
+
+
+    def __init__(self, file_name, name, ra, dec):
+        self.file_name = file_name
+        star_file = File(self.file_name)
+        self.name = name
+        self.ra = ra
+        self.dec = dec
+        self.file_text = star_file.open_file()
+        self.files = self.parse_votable(self.file_text)
+        self.get_spectra()
+
+
+
+    def get_spectra(self):
+        self.spectrum = []
+        for f in self.files:
+            # test data
+            x = np.arange(100)
+            y = np.random.rand(100)
+            self.spectrum.append(Spectrum(f, x, y))
+
+    def parse_votable(self, text):
         """ extract file names form votable """
         fits = re.findall(
             'http://ssaproxy.asu.cas.cz/getproduct/ccd700/data/.{,30}fits', text)
         return fits
 
+
+
 class Spectrum():
     """ individual spectrum of a star """
-    def __init__(self, name='', ra=0, dec=0, x=[], y=[]):
+    def __init__(self, name='', x=[], y=[]):
         self.name = name
-        self.ra = ra
-        self.dec = dec
         self.x = x
         self.y = y
 
 
 class Plot():
-    def __init__(self, spectra):
-        self.spectra = spectra
+    def __init__(self, stars):
+
+        self.stars = stars
         self.fig = plt.figure()
         self.connection_id = self.fig.canvas.mpl_connect(
             'button_press_event', self.onclick)
@@ -91,40 +143,43 @@ class Plot():
 
     def onclick(self, event):
         if event.button == 1:
-            self.spectra.next()
+            self.stars.next()
         elif event.button == 2:
             sys.exit(0)
         if event.button == 3:
-            self.spectra.previous()
+            self.stars.previous()
 
         self.click()
 
     def click(self):
 
         # test data
-        self.name = self.spectra.current.name
-        self.x = self.spectra.current.x
-        self.y = self.spectra.current.y
+        self.name = self.stars.current.name
+        self.ra = self.stars.current.ra
+        self.dec = self.stars.current.dec
+        self.x = self.stars.current.spectrum[0].x
+        self.y = self.stars.current.spectrum[0].y
         self.info = ['name', 'ra = 100', 'dec = 200']
-        print self.name, self.x, self.y
+        print self.name, self.ra, self.dec
         self.plot(self.x, self.y, self.info)
 
 class TestClass:
-    my_spectra = Spectra()
     test_file = 'votable.xml'
+    my_stars = Stars('stars.csv')
+    my_file = File(test_file)
     def test_open_file(self):
-        assert self.my_spectra.open_file(self.test_file)
+        assert self.my_file.open_file()
 
-    def test_parse_fits(self):
-        text = self.my_spectra.open_file(self.test_file)
-        fits = self.my_spectra.parse_fits(text)
+    def test_parse_votable(self):
+        text = self.my_file.open_file()
+        fits = self.my_star.parse_votable(text)
         assert len(fits) == 265
 
 
 def main():
-
-    my_spectra = Spectra()
-    my_plot = Plot(my_spectra)
+    test_file = 'votable.xml'
+    my_stars = Stars('stars.csv')
+    my_plot = Plot(my_stars)
 
     my_plot.test()
 
