b1f36a9cac2077bef2ad86735086b62f4c2f781c
astar <jaroslav.vazny@gmail.com>
2012-04-20 00:52:32 +0200

    ema separtion works

--- ema/ema.py
+++ ema/ema.py
@@ -17,6 +17,7 @@ __version__ = '1.2.3' #Versioning: http://www.python.org/dev/peps/pep-0386/
 
 
 import matplotlib.pyplot as plt
+import matplotlib.widgets as widgets
 import numpy as np
 import shutil as s
 import urllib2
@@ -36,7 +37,7 @@ class Stars():
         self.get_stars()
         self.position = 0
         self.current = self.star[self.position]
-
+        print 'current %s' % self.current.name
     def next(self):
         if self.position < len(self.csv_data) - 1:
             self.position += 1
@@ -51,6 +52,7 @@ class Stars():
 
     def get_stars(self):
         self.star = []
+#        print self.csv_data
         for d in self.csv_data:
             name = d[0]
             ra   = d[1]
@@ -71,6 +73,7 @@ class File():
     def open_file(self):
         try:
             f = open(self.name, "r+")
+            print 'openning file %s' % self.name
             text = f.read()
             f.close()
             return text
@@ -121,7 +124,6 @@ class Star():
         self.files = self.parse_votable(self.file_text)
         self.get_spectra()
 
-
     def download_spectra(self, dir, files):
         self.my_dir = Dir(dir)
         self.my_dir.remove() # delete temp dir for download with old files
@@ -133,7 +135,6 @@ class Star():
             download_name = download_name.split('?')[0] # just *.fits
             download_name = os.path.join(dir, download_name)
             self.download_names.append(download_name)
-
             try:
                     s = urllib2.urlopen(url)
                     content = s.read()
@@ -145,11 +146,14 @@ class Star():
                 print "Could not download file %s!" % f
 
     def get_spectra(self):
-
         self.download_spectra('download', self.files)
         self.spectrum = []
         for f in self.download_names:
             self.spectrum.append(Spectrum(f))
+
+    def get_votable():
+        """ Downlad votable from ssa server """
+
 
     def parse_votable(self, text):
         """ extract file names form votable """
@@ -167,7 +171,7 @@ class Spectrum():
         self.load()
 
     def load(self):
-        print self.name
+#        print self.name
         hdu = pyfits.open(self.name)
         data = hdu[1].data
         self.x = data.field('WAVE')
@@ -181,12 +185,32 @@ class Plot():
 
         self.stars = stars
         self.fig = plt.figure()
-        self.connection_id = self.fig.canvas.mpl_connect(
-            'button_press_event', self.onclick)
+        plt.autoscale(enable=True, axis='both', tight=None)
+        self.fig.subplots_adjust(left=0.25, bottom=0.25)
+#        self.connection_id = self.fig.canvas.mpl_connect(
+#            'button_press_event', self.onclick)
+        # set the maximum separation to the median of the first star
+        self.clear()
+
+        # sep slider
+        self.sep_max = round(np.median(self.stars.star[0].spectrum[0].y)*3)
+        self.axcolor = 'lightgoldenrodyellow'
+        self.axfreq = plt.axes([0.3, 0.1, 0.5, 0.03], axisbg=self.axcolor)
+        self.sep = widgets.Slider(self.axfreq, 'Sep', 0.1, 1, valinit=0.2)
+        self.sep.on_changed(self.update)
+
+
         self.click()
+
+    def update(self, val):
+        s = self.sep.val
+        self.separate(s)
+        plt.draw()
+
 
     def plot(self, x, y, info):
         self.ax.plot(x,y)
+
 
 
     def clear(self):
@@ -194,38 +218,56 @@ class Plot():
         self.ax = self.fig.add_subplot(111)
 
 
+
     def onclick(self, event):
-        if event.button == 1:
-            self.stars.next()
-        elif event.button == 2:
-            sys.exit(0)
-        if event.button == 3:
-            self.stars.previous()
+  #      if event.button == 1:
+ #           self.stars.next()
+#        elif event.button == 2:
+#            sys.exit(0)
+#        if event.button == 3:
+#            # self.stars.previous()
+#            self.separate()
 
         self.click()
 
     def click(self):
 
-
-        # test data
         self.name = self.stars.current.name
         self.ra = self.stars.current.ra
         self.dec = self.stars.current.dec
         self.info = ['name', 'ra = 100', 'dec = 200']
-        print self.name, self.ra, self.dec
-        self.clear()
 
-        for spectrum in self.stars.current.spectrum:
+
+        for i, spectrum in enumerate(self.stars.current.spectrum):
             self.x = spectrum.x
             self.y = spectrum.y
             self.plot(self.x, self.y, self.info)
+
+        self.update(self.sep.val)
         plt.show()
 
+    def separate(self, s):
+        """ Separate charts  """
+        temp_max = []
+        for i, line in enumerate(self.ax.lines):
+            line.set_ydata(self.y + i*s*self.sep_max)
+            temp_max.append(np.max(line.get_ydata()))
 
-class TestClass:
-    test_file = 'votable.xml'
-    my_stars = Stars('stars.csv')
-    my_file = File(test_file)
+
+        self.ax.set_ylim([0,np.max(temp_max)])
+
+        plt.draw()
+
+
+
+
+
+class TestClass():
+    def __init__():
+        test_file = 'votable.xml'
+        my_stars = Stars('stars.csv')
+        my_file = File(test_file)
+
     def test_open_file(self):
         assert self.my_file.open_file()
 
@@ -244,10 +286,13 @@ class TestClass:
 
 def main():
 
+
     my_stars = Stars('stars.csv')
     my_plot = Plot(my_stars)
 
-    my_plot.test()
+#    my_plot.test()
+
+    # draw the current state (relative to the last reference)
 
 
 
