669219a77c8a03512d047a78107a9fe9708aa3e7
astar <jaroslav.vazny@gmail.com>
2012-07-26 00:41:52 +0200

    script for extracking ra, dec from fits

---
+++ getradec/getradec.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+""" Prints ra,dec of a FITs file """
+import os
+import sys
+import pyfits
+
+file = sys.argv[1]
+
+if len(sys.argv) == 3:
+    secondary = sys.argv[2]
+else:
+    secondary = 0
+
+
+hdu = pyfits.open(file)
+hdr = hdu[int(secondary)].header
+print '{},{},{}'.format(os.path.basename(file), hdr['RA'], hdr['DEC'])
