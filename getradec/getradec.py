8f7037686779f0b2bb8d095dd26f7b6345610335
astar <jaroslav.vazny@gmail.com>
2012-07-29 18:16:26 +0200

    getradec.py now convert ra, dec to degree and understand different fits headers RA Vs CAT-RA etc

--- getradec/getradec.py
+++ getradec/getradec.py
@@ -1,8 +1,33 @@
 #!/usr/bin/python
 """ Prints ra,dec of a FITs file """
 import os
+import re
 import sys
 import pyfits
+import sys
+import string
+
+
+def dec2deg(inp):
+    dec=string.split(inp, ":")
+    hh=abs(float(dec[0]))
+    mm=float(dec[1])/60
+    ss=float(dec[2])/3600
+    if float(dec[0]) < 0:
+        sgn = '-'
+    else:
+        sgn = ''
+
+    return sgn + str(hh+mm+ss)
+
+def ra2deg(inp):
+    ra=string.split(inp, ":")
+    hh=abs(float(ra[0]))*15
+    mm=float(ra[1])/60*15
+    ss=float(ra[2])/3600*15
+
+
+    return  str(hh+mm+ss)
 
 file = sys.argv[1]
 
@@ -15,4 +40,14 @@ else:
 hdu = pyfits.open(file)
 hdr = hdu[int(secondary)].header
 file_info = file.replace('/',',')
-print '{},{},{},{}'.format(file_info, hdr['OBJECT'], hdr['RA'], hdr['DEC'])
+#import ipdb;ipdb.set_trace()
+
+ra = hdr['RA']
+if re.search('pyfits', str(ra)):
+    ra = hdr['CAT-RA']
+
+dec = hdr['DEC']
+if re.search('pyfits', str(dec)):
+    dec = hdr['CAT-DEC']
+
+print '{},{},{},{},{},{}'.format(file_info, hdr['OBJECT'], ra, dec,ra2deg(ra), dec2deg(dec))
