8c30c931fed301cf817319c7afdfe34e3b18e2a8
Jaroslav Vazny <jaroslav.vazny@gmail.com>
2013-02-19 03:23:15 +0100

    working click to zoom and exit button

--- ond_galery/ond_galery.py
+++ ond_galery/ond_galery.py
@@ -1,30 +1,34 @@
 import os
 import math
+import sys
 import pyfits as pf
 import dateutil as dt
 import matplotlib.pyplot as plt
-from matplotlib.ticker import NullFormatter
+import matplotlib.widgets as widgets
 
 def main():
     """
     """
-    cat = Category('trainning3/5')
+    cat = Category('trainning3/3')
     Plot(cat)
     
 class Plot():
+    def update(self, val):
+        frame = numpy.floor(sframe.val)
+        ln.set_xdata(xdata[frame])
+        ln.set_ydata((frame+1)* ydata[frame])
+        ax.set_title(frame)
+        ax.relim()
+        ax.autoscale_view()
+        plt.draw()
 
-    def __init__(self, category):
-        self.columns = 4
-        self.rows = int(math.ceil(category.count / float(self.columns)))
-        print "plot will be {} x {}".format(self.rows, self.columns)
-        #ax = [plt.subplot(self.rows, self.columns, actual) for actual in arange(category.count)]
-        fig, axes = plt.subplots(self.rows, self.columns)
-        fig.subplots_adjust(wspace=0,hspace=0)
-        
-        for index, ax in enumerate(axes.flat):
-            if len(category.stars) > index:
+    def populate_frame(self, val):
+        """
+        """
+        for index, ax in enumerate(self.axes.flat):
+            if len(self.category.stars) > index:
 
-                star = category.stars[index]
+                star = self.category.stars[index]
                 ax.plot(star.spectra[0].x, star.spectra[0].y)
                 ax.set_title = "test"
                 ax.title.set_visible(True)
@@ -37,17 +41,50 @@ class Plot():
             ax.axes.get_xaxis().set_visible(False)
             ax.axes.get_yaxis().set_visible(False)
         
-        fig.canvas.mpl_connect('button_press_event', self.on_click)
-        plt.show()
 
+        
+    def __init__(self, category):
+        self.category = category
+        self.columns = 4
+        self.rows = int(math.ceil(category.count / float(self.columns)))
+        print "plot will be {} x {}".format(self.rows, self.columns)
+        #ax = [plt.subplot(self.rows, self.columns, actual) for actual in arange(category.count)]
+        self.fig, self.axes = plt.subplots(self.rows, self.columns)
+        self.fig.subplots_adjust(wspace=0,hspace=0)
+        self.populate_frame(1)
+        self.fig.canvas.mpl_connect('button_press_event', self.on_click)
+
+        # buttons
+        self.axprev = plt.axes([0.125, 0.03, 0.1, 0.055])
+        self.axnext = plt.axes([0.225, 0.03, 0.1, 0.055])
+        self.axexit = plt.axes([0.335, 0.03, 0.1, 0.055])
+        self.bnext = widgets.Button(self.axnext, '>>')
+        self.bnext.on_clicked(self.next)
+        self.bprev = widgets.Button(self.axprev, '<<')
+        self.bprev.on_clicked(self.prev)
+        self.bexit = widgets.Button(self.axexit, '(o)(o)')
+        self.bexit.on_clicked(sys.exit)
+
+        plt.show()
+        
+    def next(self, event):
+        """
+        """
+        pass
+
+    def prev(self, event):
+        """
+        """
+        pass
     
     def on_click(self, event):
         """Enlarge or restore the selected axis."""
         ax = event.inaxes
+
         if ax is None:
             # Occurs when a region not in an axis is clicked...
             return
-        if event.button is 1:
+        if event.button is 1 and ax.has_data():
             # On left click, zoom the selected axes
             ax._orig_position = ax.get_position()
             ax.set_position([0.1, 0.1, 0.85, 0.85])
