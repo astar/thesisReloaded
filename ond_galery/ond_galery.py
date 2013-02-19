4d73b2a6545289f3e34516a126a3119b0f588c79
Jaroslav Vazny <jaroslav.vazny@gmail.com>
2013-02-19 02:12:50 +0100

    before frame experiment

--- ond_galery/ond_galery.py
+++ ond_galery/ond_galery.py
@@ -8,7 +8,7 @@ from matplotlib.ticker import NullFormatter
 def main():
     """
     """
-    cat = Category('trainning3/3')
+    cat = Category('trainning3/5')
     Plot(cat)
     
 class Plot():
@@ -37,11 +37,37 @@ class Plot():
             ax.axes.get_xaxis().set_visible(False)
             ax.axes.get_yaxis().set_visible(False)
         
-
+        fig.canvas.mpl_connect('button_press_event', self.on_click)
         plt.show()
 
-
-
+    
+    def on_click(self, event):
+        """Enlarge or restore the selected axis."""
+        ax = event.inaxes
+        if ax is None:
+            # Occurs when a region not in an axis is clicked...
+            return
+        if event.button is 1:
+            # On left click, zoom the selected axes
+            ax._orig_position = ax.get_position()
+            ax.set_position([0.1, 0.1, 0.85, 0.85])
+            for axis in event.canvas.figure.axes:
+                # Hide all the other axes...
+                if axis is not ax:
+                    axis.set_visible(False)
+        elif event.button is 3:
+            # On right click, restore the axes
+            try:
+                ax.set_position(ax._orig_position)
+                for axis in event.canvas.figure.axes:
+                    axis.set_visible(True)
+            except AttributeError:
+                # If we haven't zoomed, ignore...
+                pass
+        else:
+            # No need to re-draw the canvas if it's not a left or right click
+            return
+        event.canvas.draw()
 
 class Category(list):
     
