b7b33708753ca7523c46be85b1b4248f808aaa9e
Jaroslav Vazny <jaroslav.vazny@gmail.com>
2013-03-05 04:41:55 +0100

    changed header.py to accept key value

--- header/header.py
+++ header/header.py
@@ -5,12 +5,9 @@ import pyfits
 
 file = sys.argv[1]
 
-if len(sys.argv) == 3:
-    secondary = sys.argv[2]
-else:
-    secondary = 0
-
-print file
 hdu = pyfits.open(file)
-print hdu[int(secondary)].header
 
+if len(sys.argv) == 3:
+    print hdu[0].header[sys.argv[2]]
+else:
+    print hdu[0].header
